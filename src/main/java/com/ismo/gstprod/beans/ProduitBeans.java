46a1f5b2b6068077cdbe8c09ecabe28e06a0a863
lamnaourm <mohammed.lamnaour@gmail.com>
2022-04-30 14:26:48 +0000

    suppression

--- src/main/java/com/ismo/gstprod/beans/ProduitBeans.java
+++ src/main/java/com/ismo/gstprod/beans/ProduitBeans.java
@@ -2,7 +2,9 @@ package com.ismo.gstprod.beans;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,9 +18,23 @@ public class ProduitBeans {
 
 	@Autowired
 	IMetier<Produit> metierProduit;
-	
-	
-	public List<Produit> getAllproduit(){
+
+	public List<Produit> getAllproduit() {
 		return metierProduit.getAll();
+	}
+
+	public void deleteProduit(int id) {
+		Produit p = new Produit();
+		p.setNumProd(id);
+		
+		if(metierProduit.delete(p))
+			addMessage("Confirmed", "Produit supprime avec succes");
+		else
+			addMessage("error", "Erreur de suppression");
+	}
+
+	public void addMessage(String summary, String detail) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
 }
