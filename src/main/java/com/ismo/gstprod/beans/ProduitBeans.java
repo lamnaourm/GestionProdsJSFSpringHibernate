3673c72035996d5e6106bcc0608056c1e5f4d2b3
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-07 20:57:39 +0000

    update produit

--- src/main/java/com/ismo/gstprod/beans/ProduitBeans.java
+++ src/main/java/com/ismo/gstprod/beans/ProduitBeans.java
@@ -21,6 +21,7 @@ public class ProduitBeans {
 
 	@Autowired
 	IMetier<Produit> metierProduit;
+	ModeEditProduit mode = ModeEditProduit.ADD;
 	
 	private Produit newProd=new Produit();
 
@@ -56,22 +57,42 @@ public class ProduitBeans {
 	}
 	
 	public void updateProduit(int id) {
-		
 		newProd = metierProduit.getOne(id);
+		mode = ModeEditProduit.UPDATE;
 		PrimeFaces current = PrimeFaces.current();
 		current.executeScript("PF('dialog1').show()");
 	}
 	
 	
-	public void addProduit() {
-		if(metierProduit.save(newProd)) {
-			addMessage(FacesMessage.SEVERITY_INFO,"Confirmed", "Produit ajoute avec succes");
-		}else 
-			addMessage(FacesMessage.SEVERITY_ERROR,"Confirmed", "Echec d'ajout");
+	public void addUpdateProduit() {
+		
+		switch (mode) {
+		case ADD:
+			if(metierProduit.save(newProd)) {
+				addMessage(FacesMessage.SEVERITY_INFO,"Confirmed", "Produit ajoute avec succes");
+			}else 
+				addMessage(FacesMessage.SEVERITY_ERROR,"Confirmed", "Echec d'ajout");
+			break;
+		case UPDATE:
+			if(metierProduit.update(newProd)) {
+				addMessage(FacesMessage.SEVERITY_INFO,"Confirmed", "Produit modifie avec succes");
+			}else 
+				addMessage(FacesMessage.SEVERITY_ERROR,"Confirmed", "Echec de modification");
+			mode = ModeEditProduit.ADD;			
+			break;
+
+		}
+		
 	}
 
 	public void addMessage(FacesMessage.Severity severity,String summary, String detail) {
 		FacesMessage message = new FacesMessage(severity, summary, detail);
 		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
+}
+
+
+enum ModeEditProduit{
+	ADD,
+	UPDATE
 }
